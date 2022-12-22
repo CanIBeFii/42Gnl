970284c60288fcee58c17c604b1d531e72ae426d
CanIBeFii <filipe250502@gmail.com>
2022-12-22 12:26:09 +0000

    added gnl main functions

--- get_next_line.c
+++ get_next_line.c
@@ -3,11 +3,101 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: filipe <filipe@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 12:11:43 by filipe            #+#    #+#             */
-/*   Updated: 2022/12/21 12:58:05 by filipe           ###   ########.fr       */
+/*   Updated: 2022/12/22 12:22:59 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+char	*get_next_line(int file_descriptor)
+{
+	static char	*string;
+	char		*string_tmp1;
+	char		*string_tmp2;
+
+	while (ft_findchar(string, '\n') == -1)
+	{
+		string_tmp1 = string;
+		string_tmp2 = ft_gnl_read(file_descriptor);
+		if (string_tmp2 == NULL)
+			return (string);
+		string = ft_strjoin(string_tmp1, string_tmp2);
+		if (string_tmp1 != NULL)
+			free(string_tmp1);
+		if (string_tmp2 != NULL)
+			free(string_tmp2);
+	}
+	string_tmp1 = ft_strdup(string);
+	string_tmp1 = get_before_nl(string_tmp1);
+	string = get_after_nl(string);
+	return (string_tmp1);
+}
+
+char	*get_before_nl(char *string)
+{
+	char	*return_string;
+	int		index;
+	int		counter;
+
+	index = ft_findchar(string, '\n');
+	if (index == -1)
+		return (string);
+	return_string = malloc(sizeof(char) * (index + 1));
+	if (return_string == NULL)
+		return (NULL);
+	counter = 0;
+	while (counter < index)
+	{
+		return_string[counter] = string[counter];
+		counter++;
+	}
+	return_string[counter] = '\0';
+	free(string);
+	return (return_string);
+}
+
+char	*get_after_nl(char *string)
+{
+	char	*temporary_string;
+	int		size;
+	int		start;
+
+	if (string == NULL)
+		return (NULL);
+	size = ft_strlen(string);
+	start = 0;
+	while (string[start] && string[start] != '\n')
+		start++;
+	if (string[start] == '\n')
+		start++;
+	temporary_string = ft_substr(string, start, (size - start));
+	if (temporary_string[0] == '\0')
+	{
+		free(string);
+		free(temporary_string);
+		return (NULL);
+	}
+	free(string);
+	return (temporary_string);
+}
+
+char	*ft_gnl_read(int file_descriptor)
+{
+	char	*string;
+	size_t	size_read;
+
+	string = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (string == NULL)
+		return (NULL);
+	size_read = read(file_descriptor, string, BUFFER_SIZE);
+	if (size_read <= 0 || size_read > BUFFER_SIZE)
+	{
+		free(string);
+		return (NULL);
+	}
+	string[size_read] = '\0';
+	return (NULL);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fialexan <fialexan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/21 12:14:21 by filipe            #+#    #+#             */
-/*   Updated: 2022/12/22 11:33:13 by fialexan         ###   ########.fr       */
+/*   Updated: 2022/12/22 12:24:30 by fialexan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,8 @@ char	*get_next_line(int fd);
  * @brief Finds the first char C in STRING and returns it's, 
  * if there's none returns 0.
  * 
- * @param string
- * @param c 
+ * @param string  char*
+ * @param c  char
  * @return int 
  */
 int		ft_findchar(char *string, char c);
@@ -34,7 +34,7 @@ int		ft_findchar(char *string, char c);
  * @brief Finds the length of the string and returns it,
  *  if the string is not allocated returns 0.
  * 
- * @param string 
+ * @param string char*
  * @return int 
  */
 int		ft_strlen(char *string);
@@ -42,8 +42,8 @@ int		ft_strlen(char *string);
 /**
  * @brief Joins the string1 and the string2.
  * 
- * @param string1 
- * @param string2 
+ * @param string1 char*
+ * @param string2 char*
  * @return char* 
  */
 char	*ft_strjoin(char *string1, char *string2);
@@ -51,7 +51,7 @@ char	*ft_strjoin(char *string1, char *string2);
 /**
  * @brief Creates a duplicate of string and allocates the memory needed.
  * 
- * @param string 
+ * @param string char*
  * @return char* 
  */
 char	*ft_strdup(char *string);
@@ -60,9 +60,9 @@ char	*ft_strdup(char *string);
  * @brief Creates a new string(sub_string) that copys 'length'
  *  characters of 'string' starting from 'start'. 
  * 
- * @param string 
- * @param start 
- * @param length 
+ * @param string char*
+ * @param start int
+ * @param length int
  * @return char* 
  */
 char	*ft_substr(char *string, int start, int length);
